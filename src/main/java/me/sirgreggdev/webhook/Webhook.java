0aa13865949348b8aef948c1e0d0ed23ef4980fb
Gregg <greggkrzykowski@gmail.com>
2017-10-31 15:55:57 -0500

    basic send message

--- src/main/java/me/sirgreggdev/webhook/Webhook.java
+++ src/main/java/me/sirgreggdev/webhook/Webhook.java
@@ -1,10 +1,25 @@
 package me.sirgreggdev.webhook;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+
 public class Webhook {
     private String url;
     public Webhook(String url) {
         this.url = url;
     }
 
+    public void sendMessage(String message) {
+        try {
+            HttpResponse<String> res = Unirest.post(url)
+                    .header("Content-Type", "application/json")
+                    .body(new JSONObject().put("content", message)).asString();
 
+            System.out.println(res.getBody());
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
 }
