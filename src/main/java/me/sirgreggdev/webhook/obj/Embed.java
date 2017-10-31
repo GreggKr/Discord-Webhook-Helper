5c6df83e5f2dd5d92f1d4fdbb8e7a05eeab6ab1b
Gregg <greggkrzykowski@gmail.com>
2017-10-31 17:18:32 -0500

    Super basic builder pattern just for testing

--- src/main/java/me/sirgreggdev/webhook/obj/Embed.java
+++ src/main/java/me/sirgreggdev/webhook/obj/Embed.java
@@ -1,8 +1,10 @@
 package me.sirgreggdev.webhook.obj;
 
+import org.json.JSONObject;
+
 public class Embed {
     private String title;
-    private String type = "rich";
+    private static final String TYPE = "rich";
     private String description;
     private String url;
     private String timestamp;
@@ -14,4 +16,23 @@ public class Embed {
     private Provider provider;
     private Author author;
     private Field[] fields;
+
+    public Embed setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Embed setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public JSONObject build() {
+        JSONObject object = new JSONObject();
+        if (title != null) object.put("title", title);
+        object.put("type", TYPE);
+        object.put("description", description);
+
+        return object;
+    }
 }
