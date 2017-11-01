a1ce891a4c849b7b593b4ebe3f6b22ec4c13729a
Gregg <greggkrzykowski@gmail.com>
2017-11-01 08:09:54 -0500

    More work on Embed

--- src/main/java/me/sirgreggdev/webhook/obj/Embed.java
+++ src/main/java/me/sirgreggdev/webhook/obj/Embed.java
@@ -27,11 +27,30 @@ public class Embed {
         return this;
     }
 
+    public Embed setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public Embed setColor(int color) {
+        this.color = color;
+        return this;
+    }
+
+    public Embed setFooter(Footer footer) {
+        this.footer = footer;
+        return this;
+    }
+
     public JSONObject build() {
+        if (description == null) return null;
+
         JSONObject object = new JSONObject();
         if (title != null) object.put("title", title);
         object.put("type", TYPE);
         object.put("description", description);
+        if (url != null) object.put("url", url);
+
 
         return object;
     }
