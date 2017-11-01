e2c7b19b9b3e6c16750216b2c5dcff77d72d497b
Gregg <greggkrzykowski@gmail.com>
2017-11-01 08:09:46 -0500

    More work on webhookdata

--- src/main/java/me/sirgreggdev/webhook/obj/WebhookData.java
+++ src/main/java/me/sirgreggdev/webhook/obj/WebhookData.java
@@ -16,6 +16,26 @@ public class WebhookData {
         return this;
     }
 
+    public WebhookData setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public WebhookData setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+        return this;
+    }
+
+    public WebhookData setTts(boolean tts) {
+        this.tts = tts;
+        return this;
+    }
+
+    public WebhookData setEmbeds(Embed[] embeds) {
+        this.embeds = embeds;
+        return this;
+    }
+
     public JSONObject build() {
         JSONObject object = new JSONObject();
         object.put("content", content);
